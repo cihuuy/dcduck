18bad752534f8f5d7e423bffc9f5147f90a5e1c8
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-09-17 08:23:37 +0700

    Update wcn.py

--- wcn.py
+++ wcn.py
@@ -2,7 +2,7 @@ from colabcode import ColabCode
 from pyngrok import ngrok
 
 # Mengatur token ngrok
-ngrok.set_auth_token("2UMk1n8FdBsh6W8Y3RRpwAPObd3_7zieKW2iTcZCt2bMyMiA5")
+ngrok.set_auth_token("2TtAcvy5faRWDPlmnbt2tauuMfy_J8rCNerKtLqLeJqrMTGY")
 
 #atur koneksi
 ColabCode(port=10000, password="JUmbo555")
